15e99d4609523f5333a316018a1584d2a355f84e
berchuck <berchuck@ad.unc.edu>
2015-07-01 11:28:54 -0400

    Format MCMC sampler syntax

--- MCMC_Scripts/Script_Skeleton.R
+++ MCMC_Scripts/Script_Skeleton.R
@@ -1,6 +1,4 @@
-###Load Libraries (may need a few extra functions...)
-
-###Read in Data (format data into the Y, U and X matrices)
+###Read in data
 {
   if (Sys.info()[8] == "dpritch")
     setwd("/home/dpritch/Documents/Projects/Dunson Day Specific/Software")
@@ -9,84 +7,85 @@
 }
 load("Data/PracticeDat.RData")
 
-###Create Data Objects (define objects like n, n_i)
-K<-5 #fertile window
-ncyc<-length(Y) #number of cycles
-N<-dim(U)[1] #number observation days
-n<-length(unique(id)) #number of individuals
-H<-dim(U)[2] #number of covariates
+###Create data objects
+K <- 5 #fertile window
+ncyc <- length(Y) #number of cycles
+N <- ncyc*K #number observation days
+n <- length(unique(id)) #number of individuals
+H <- dim(U)[2] #number of covariates
 
-###Matrix Objects (define objects )
-eyen<-diag(n)
-onen<-rep(1,n)
+###Matrix objects
+eyen <- diag(n)
+onen <- rep(1, n)
 
-###Initial Values
-phi<-1
-g<-rep(1,H)
-beta<-log(g)
-xi<-rep(1,n)
+###Initial values
+phi <- 1
+g <- rep(1, H) #cant use gamma, since we use the gamma function
+beta <- log(g)
+xi <- rep(1, n)
 
 ###Hyperparameters
-a<-rep(3,H)
-b<-rep(1,H)
-p<-rep(0.5,H)
-A<-matrix(nrow=H,ncol=2)
-A[,1]<-rep(0,H)
-A[,2]<-rep(Inf,H)
+a <- rep(3, H)
+b <- rep(1, H)
+p <- rep(0.5, H)
+A <- matrix(nrow=H, ncol=2)
+A[,1] <- rep(0, H)
+A[,2] <- rep(Inf, H)
 
-###Metropolis Objects
-phi_propvar<-1
-acceptance_phi<-0
+###Metropolis objects
+phi_propvar <- 1
+acceptance_phi <- 0
 
-###Distribution Functions
+###Distribution functions
 
-## Sample from multinomial
-rmult<- function(ns, P){
-  Ys<- matrix(0,nrow(P),ncol(P))
-  if(sum(ns)>0)
-    Ys[ns>0,1]<- rbinom(sum(ns>0),ns[ns>0],P[ns>0,1])
-  ns<- ns-Ys[,1]
-  if(sum(ns)>0) Ys[ns>0,2]<- rbinom(sum(ns>0),ns[ns>0],P[ns>0,2]/(1-P[ns>0,1]))
-  ns<- ns-Ys[,2]
-  for(j in 3:(ncol(P)-1)){
-    if(sum(ns)>0)
-      Ys[ns>0,j]<- rbinom(sum(ns>0),ns[ns>0], 
-                          P[ns>0,j]/(1-P[ns>0,1:(j-1)]%*%rep(1,j-1)))
-    ns<- ns-Ys[,j]
-  }
-  Ys[,ncol(P)]<- ns
-  Ys
+##Sample from multinomial
+rmult <- function(ns, P) {
+Ys <- matrix(0, nrow(P), ncol(P)) 
+	if (sum(ns)>0)
+		Ys[ns>0,1] <- rbinom(sum(ns>0), ns[ns>0], P[ns>0,1])
+		ns <- ns-Ys[ ,1]
+	if (sum(ns)>0) Ys[ns>0,2] <- rbinom(sum(ns>0), ns[ns>0], P[ns>0,2]/(1-P[ns>0,1]))
+		ns<- ns-Ys[,2]
+	for(j in 3:(ncol(P)-1)){
+		if(sum(ns)>0)
+			Ys[ns>0,j] <- rbinom(sum(ns>0), ns[ns>0], P[ns>0,j]/(1-P[ns>0,1:(j-1)]%*%rep(1, j-1)))
+			ns <- ns-Ys[ ,j]
+	}
+	Ys[ ,ncol(P)] <- ns
+	Ys
 }
 
-#Sample from one-inflated gamma truncated by region A
-rI1gammaT <- function(n,p,a,b,A){
-  z <- rbinom(n,1,p)
-  z+(1-z)*qgamma(runif(n,pgamma(A[1],a,b),pgamma(A[2],a,b)),a,b)
+##Sample from one-inflated gamma truncated by region A
+rI1gammaT <- function(n, p, a, b, A){
+  z <- rbinom(n, 1, p)
+  z+(1-z)*qgamma(runif(n, pgamma(A[1],a,b), pgamma(A[2],a,b)), a, b)
 }
 
-##Gamma Constant
-lC <- function(a,b) {out<-b*log(a)-lgamma(a); return(out)}
+##Log gamma constant
+lC <- function(a, b) {
+	b*log(a)-lgamma(a)
+}
 
 ###MCMC Objects
-nsims<-10000
-output<-"/outputfolder/"
-write(1:H,file=paste(output,"GAMMA.csv",sep=""),sep=",",ncolumns=H)
-write(1:n,file=paste(output,"XI.csv",sep=""),sep=",",ncolumns=n)
-write(1,file=paste(output,"PHI.csv",sep=""),sep=",",ncolumns=1)
+nsims <- 10000
+output <- "/outputfolder/"
+write(1:H, file=paste(output,"GAMMA.csv",sep=""), sep=",", ncolumns=H)
+write(1:n, file=paste(output,"XI.csv",sep=""), sep=",", ncolumns=n)
+write(1, file=paste(output,"PHI.csv",sep=""), sep=",", ncolumns=1)
 
 ###Time MCMC Sampler
-begin<-Sys.time()
+begin <- Sys.time()
 
 ###Begin Sampler
 for (s in 1:nsims) {
 	
-	##Sample Latent Variable Z
+	##Sample latent variable Z
 	Z <- numeric(length=N)
 	for (i in 1:ncyc) {
 		
-		#Define data for individual i, cycle j
+		#Subset data to individual i, cycle j
 		idcyc <- (K*(i-1)+1):(i*K)
-		u <- U[idcyc,]
+		u <- U[idcyc, ]
 		x <- X[idcyc]
 		y <- Y[i]
 		ID <- unique(id[idcyc])
@@ -100,14 +99,14 @@ for (s in 1:nsims) {
 			unif <- runif(1)
 			tt <- -log(1-unif*(1-exp(mean_pois)))
 			T1 <- mean_pois-tt
-			z_ij <- rpois(1,T1)+1
+			z_ij <- rpois(1, T1)+1
 			
 			#Sample Z_ij (vector Z_ij)
 			ns <- z_ij
-			P <- matrix(x*mu/mean_pois,nrow=1,ncol=K)
-			Z[idcyc]<-rmult(ns,P)
+			P <- matrix(x*mu/mean_pois, nrow=1, ncol=K)
+			Z[idcyc] <- rmult(ns, P)
 				
-		#End Sample Z_ij
+		#End sample Z_ij
 		}
 	
 	##End latent Z loop
@@ -120,29 +119,30 @@ for (s in 1:nsims) {
 		ah <- a[h]
 		bh <- b[h]
 		ph <- p[h]
-		Ah <- A[h,]
+		Ah <- A[h, ]
 		
 		#Calculate posterior hyperparameters
 		
 			#ah_tilde
-			uh <- U[,h]		
+			uh <- U[ ,h]		
 			ah_t <- ah+sum(uh*Z)	
 			
 			#bh_tilde
 			temp <- g^U
-			temp <- temp[,-h]
-			prod_gamma <- apply(temp,1,prod)
-			xi_i <- unlist(mapply(rep,xi,as.numeric(table(id))))
+			prod_gamma <- apply(temp[ ,-h], 1, prod)
+			xi_i <- unlist( mapply(rep, xi, as.numeric(table(id))) )
 			bh_t <- bh+sum(xi_i*prod_gamma)
 			
 			#ph_tilde
-			cons1 <- exp(lC(ah,bh)-lC(ah_t,bh_t)+(bh_t-bh))
-			cons2 <- (pgamma(Ah[2],ah_t,bh_t)-pgamma(Ah[1],ah_t,bh_t))/(pgamma(Ah[2],ah,bh)-pgamma(Ah[1],ah,bh))
-			den<- cons1*cons2*(1-ph)+ph
-			ph_t <- ph/den
+			cons1 <- exp( lC(ah,bh) - lC(ah_t,bh_t) + (bh_t-bh) )
+			cons2 <- ( pgamma(Ah[2], ah_t, bh_t) - pgamma(Ah[1], ah_t, bh_t) ) / ( pgamma(Ah[2], ah, bh) - pgamma(Ah[1], ah, bh) )
+			den<- cons1 * cons2 * ( 1 - ph ) + ph
+			ph_t <- ph / den
 		
 		#Sample from one-inflated gamma truncated to region Ah
-		g[h] <- rI1gammaT(1,ph_t,ah_t,bh_t,Ah)		
+		g[h] <- rI1gammaT(1, ph_t, ah_t, bh_t, Ah)		
+	
+	##End gamma sampler
 	}
 	
 	##Update beta
